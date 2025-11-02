406715e4df3246efdfadc884c0349f0289144513
manoj <manoj.gardi@theadroit.in>
2025-11-02 11:53:24 +0530

    link separate js and css.

---
+++ js/comman.js
@@ -0,0 +1,12 @@
+// Scroll Animation Script
+const sections = document.querySelectorAll('.hidden-section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-section');
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => observer.observe(section));
